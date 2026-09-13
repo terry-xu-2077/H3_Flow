937a501402b31e6d9b04ae733d64d9943cec1dbf
Terry <49803399+terry-xu-2077@users.noreply.github.com>
2026-09-13 15:07:27 +0800

    style: load V0.7 control refinements

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -16,6 +16,7 @@ import "./styles/task-editor-polish.css";
 import "./styles/v0.6-project-prompt.css";
 import "./styles/v0.6-project-config-polish.css";
 import "./styles/v0.6-project-config-height-fix.css";
+import "./styles/v0.7-controls-polish.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
