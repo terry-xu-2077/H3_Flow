7d62a34e3ba794ee410c0e70b07b6673a64a8343
Terry <49803399+terry-xu-2077@users.noreply.github.com>
2026-09-13 12:08:37 +0800

    style(ui): load simplified editor styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,6 +10,7 @@ import "./styles/composer.css";
 import "./styles/overlays.css";
 import "./styles/director.css";
 import "./styles/director-dialogs.css";
+import "./styles/simple-editor.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
